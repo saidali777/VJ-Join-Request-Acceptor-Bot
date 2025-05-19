3172f09bb72c6db665a93f029914db274b1dca29
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-19 20:25:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ API_HASH = environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7934428209:AAF4VkZbSNnfK_JD1lHGXoEPu9zAo7kISB0")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002608373784"))
+ADMINS = int(environ.get("ADMINS", "7837304801"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
