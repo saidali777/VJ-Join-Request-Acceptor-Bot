9cdba4e71aed7585057513d598e7386a6771bd64
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-19 20:49:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28243586"))
-API_HASH = environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7566645218:AAESBbzZYtnQDLb18QrSMdZR3-HxKWkrSUY")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002328437358"))
-ADMINS = int(environ.get("ADMINS", "7357726710"))
+API_ID = int(environ.get("API_ID", "12618934"))
+API_HASH = environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7934428209:AAF4VkZbSNnfK_JD1lHGXoEPu9zAo7kISB0")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002608373784"))
+ADMINS = int(environ.get("ADMINS", "7837304801"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://madarazbotz:aemHxtb49hACpjd6@cluster0.ed7e2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "Cluster0")
